70ad6caa9ae5367089d1af2dc333500cfce72036
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2023-09-18 01:20:33 +0600

    Updated UI

--- header.php
+++ header.php
@@ -91,7 +91,7 @@ if (!defined('ABSPATH')) {
                             </a>
                         </li>
                         <li>
-                            <a href="<?php echo site_url("/cart") ?>" class="relative flex justify-center items-center <?php echo is_cart() ? "text-primary-500 dark:text-white" : "" ?>">
+                            <a href="<?php echo site_url("/cart") ?>" class="relative flex justify-center items-center">
                                 <svg class="mr-1.5" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M16.651 6.5984C16.651 4.21232 14.7167 2.27799 12.3307 2.27799C11.1817 2.27316 10.0781 2.72619 9.26387 3.53695C8.44968 4.3477 7.992 5.44939 7.992 6.5984M16.5137 21.5H8.16592C5.09955 21.5 2.74715 20.3924 3.41534 15.9348L4.19338 9.89359C4.60528 7.66934 6.02404 6.81808 7.26889 6.81808H17.4474C18.7105 6.81808 20.0469 7.73341 20.5229 9.89359L21.3009 15.9348C21.8684 19.889 19.5801 21.5 16.5137 21.5Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                                     <path d="M15.296 11.102H15.251" stroke="#2D2D2D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
