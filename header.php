44b3ca0bc2973fc9af070c3d47bed7c10bf7341e
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2023-10-18 20:40:18 +0600

    Updated UI

--- header.php
+++ header.php
@@ -75,7 +75,13 @@ if (!defined('ABSPATH')) {
         </div>
         <div class="relative md:block hidden py-4">
             <div class="relative h-auto w-full">
-                <nav class="w-full relative flex justify-start">
+                <nav class="w-full relative flex items-center justify-start space-x-2">
+                    <a class="bg-slate-100 p-2 rounded font-medium leading-5 text-zinc-600 hover:text-zinc-900 " href="<?php echo site_url("/") ?>">
+                        <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                        </svg>
+
+                    </a>
                     <?php
                     wp_nav_menu( // show nav
                         array(
